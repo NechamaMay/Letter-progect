aa87b8dc81bfaf47f940533fe7f09e5333d1e540
NechamaMay <152605523+NechamaMay@users.noreply.github.com>
2024-07-08 23:05:36 +0300

    .

--- cutsPictures.py
+++ cutsPictures.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from PIL import Image
+import os
 def split_handwritten_text(image_path, output_dir):
  # קרא את התמונה
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -16,20 +17,36 @@ def split_handwritten_text(image_path, output_dir):
         x, y, w, h = cv2.boundingRect(contour)
         letter_crop = img[y:y+h, x:x+w]
         # בודק גודל מינימלי
-      #   if w < 10 or h < 30:
-      #       continue
-      #    # שינוי גודל התמונה לגודל מקסימלי של 224x224 תוך שמירה על יחס גובה-רוחב
-      #   letter_pil = Image.fromarray(letter_crop)
-      #   letter_pil.thumbnail((224, 224))
-      #   #חותך את האות
-      #   letter_img = Image.fromarray(letter_crop)
-      #     # צור תמונה בגודל 224x224 עם רקע לבן
-      #   letter_img = Image.new("L", (224, 224), 255)
-      #   # מחשב את מיקום האות במרכז הרקע הלבן
-      #   offset = ((224 - letter_pil.width) // 2, (224 - letter_pil.height) // 2)
+        if w < 10 or h < 10:
+            continue
+         # שינוי גודל התמונה לגודל מקסימלי של 224x224 תוך שמירה על יחס גובה-רוחב
+        letter_pil = Image.fromarray(letter_crop)
+        letter_pil.thumbnail((224, 224))
+        #חותך את האות
+        letter_img = Image.fromarray(letter_crop)
+          # צור תמונה בגודל 224x224 עם רקע לבן
+        letter_img = Image.new("L", (224, 224), 255)
+        # מחשב את מיקום האות במרכז הרקע הלבן
+        offset = ((224 - letter_pil.width) // 2, (224 - letter_pil.height) // 2)
         
-      # #   # הדבק את האות במרכז הרקע הלבן
-      # #   letter_img.paste(letter_pil, offset)
-        letter_crop.ravel(f"{output_dir}/letter_{i}.png")
-split_handwritten_text(r"C:\Users\necha\Desktop\LetterProject\Libby.jpg", r"C:\Users\necha\Desktop\LetterProject\cuts")
+        # הדבק את האות במרכז הרקע הלבן
+        letter_img.paste(letter_pil, offset)
+        letter_img.save(f"{output_dir}/letter_{i}.jpg")
+import os
 
+for i in range(1, 26):
+    # בניית הנתיב של התיקיה שבה הקבצים נמצאים
+    directory_path = r"C:\Users\necha\Desktop\LetterProject\dataset\train\{}".format(i)
+    
+    try:
+        # קבלת רשימת כל הקבצים בתיקיה
+        files = os.listdir(directory_path)
+    except FileNotFoundError:
+        print(f"Directory not found: {directory_path}")
+        continue
+    
+    # הדפסת הנתיב של כל קובץ והפעלת הפונקציה split_handwritten_text
+    for file_name in files:
+        file_path = os.path.join(directory_path, file_name)
+        output_directory = r"C:\Users\necha\Desktop\LetterProject\dataset\train\{}.1".format(i)
+        split_handwritten_text(file_path, output_directory)
